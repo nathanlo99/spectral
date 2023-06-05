d93c9af4b881728520255bf71acee1879dc12caf
Nathan Lo <nathanlo99@hotmail.ca>
2023-06-04 22:24:43 -0700

    Periodically create progress snapshots

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,7 @@ vec3 ray_colour(RNG &random, const size_t remaining_depth, const Ray &r,
 
 int main() {
   RGBImage image(640, 480);
+  RGBVarianceImage variance_image(image.m_width, image.m_height);
   Camera camera(vec3(0.0, 0.0, 1.5), vec3(0.0, 0.0, 0.0));
   camera.vertical_fov = 70;
   camera.set_output_image(image);
@@ -53,11 +54,13 @@ int main() {
   scene.add(std::make_shared<Sphere>(vec3(0.0, -100.5, 0.0), 100.0,
                                      diffuse_material));
 
-  const size_t max_depth = 50, samples_per_pixel = 100;
+  const size_t max_depth = 50, samples_per_pixel = 500;
   const size_t total_samples =
       image.m_width * image.m_height * samples_per_pixel;
   size_t num_samples = 0;
-  const Timer timer;
+  size_t num_progress_updates = 0;
+
+  Timer timer;
   for (size_t row = 0; row < image.m_height; ++row) {
     for (size_t col = 0; col < image.m_width; ++col) {
       const vec2 pixel = vec2(col + 0.5, row + 0.5);
@@ -65,21 +68,33 @@ int main() {
       for (size_t sample = 0; sample < samples_per_pixel; ++sample) {
         const vec2 jitter = random.random_vec2(-0.5, 0.5);
         const Ray ray = camera.get_ray(pixel + jitter);
-        image.add_pixel_sample(row, col,
-                               ray_colour(random, max_depth, ray, scene));
+        const vec3 colour = ray_colour(random, max_depth, ray, scene);
+        image.add_pixel_sample(row, col, colour);
+        variance_image.add_pixel_sample(row, col, colour);
         ++num_samples;
       }
     }
 
+    if (timer.seconds_since_last_update() >= 1.0) {
+      timer.update();
+      image.write_png<true>("output/progress.png");
+      num_progress_updates++;
+    }
+
     fmt::print("\33[2K\rProgress: {:.2f}% [sample {}/{}, elapsed_time: "
-               "{:.2f}s, samples_per_sec: {:.2f}M]",
+               "{:.2f}s, samples_per_sec: {:.2f}M, num_progress_updates: {}]",
                100.0 * num_samples / total_samples, num_samples, total_samples,
                timer.elapsed_seconds(),
-               num_samples / timer.elapsed_seconds() / 1e6);
+               num_samples / timer.elapsed_seconds() / 1e6,
+               num_progress_updates);
     std::cout << std::flush;
   }
 
+  image.write_png<true>("output/progress.png");
   image.write_png<true>("output/result.png");
+
+  // Gamma-correction approximates a sqrt, so variance becomes stddev
+  variance_image.write_png<true>("output/stddev.png");
 
   const auto elapsed_seconds = timer.elapsed_seconds();
   fmt::println("\nDone! Took {:.2f} seconds.", elapsed_seconds);
--- src/util/timer.hpp
+++ src/util/timer.hpp
@@ -5,11 +5,23 @@
 
 struct Timer {
   std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+  std::chrono::time_point<std::chrono::high_resolution_clock> last_update_time;
 
-  Timer() : start_time(std::chrono::high_resolution_clock::now()) {}
+  Timer()
+      : start_time(std::chrono::high_resolution_clock::now()),
+        last_update_time(start_time) {}
 
   double elapsed_seconds() const {
     const auto end_time = std::chrono::high_resolution_clock::now();
     return std::chrono::duration<double>(end_time - start_time).count();
+  }
+
+  double seconds_since_last_update() const {
+    const auto now_time = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration<double>(now_time - last_update_time).count();
+  }
+
+  void update() {
+    last_update_time = std::chrono::high_resolution_clock::now();
   }
 };
